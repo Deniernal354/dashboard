44fce865cc102335a48b964aff341e70202a77d8
ghyoo91 <ghyoo91@naver.com>
2018-01-30 10:49:47 +0900

    1st!

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true,
+        "jquery":true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": ["off", { "varsIgnorePattern": "^h$" }],
+        "no-console" : "off"
+    },
+    "globals":{
+      "NProgress": false,
+      "PNotify": false,
+      "Chart": false
+    }
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+Dashboard
+
+Lang : Node.js
+DB : mysql
+Main Module : Express, Gulp, Chart.js
+Detail : Dashboard for Test Automation Results
