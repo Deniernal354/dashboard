27cf79deabaf2c53287636eadca120ffeb7d95ed
ghyoo91 <ghyoo91@naver.com>
2019-01-10 15:27:45 +0900

    Test Commit after rebase

--- src/js/custom.js
+++ src/js/custom.js
@@ -36,11 +36,6 @@
   };
 })(jQuery, "smartresize");
 
-/**
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates and open the template in the editor.
- */
-
 // First written by Ariel
 // https://stackoverflow.com/questions/12274748/
 function setAttributes(el, attrs) {
